40814bcad3913ae32f92c915f251626d9000d5d8
shrutibalan4591 <77207245+shrutibalan4591@users.noreply.github.com>
2021-06-08 15:06:50 +0530

    Update app.py

--- app.py
+++ app.py
@@ -1,24 +1,23 @@
 import pickle
-import numpy as np
 from flask import Flask, request, render_template
 
 app = Flask(__name__)
-model = pickle.load(open("model.pkl", "rb"))
+model = pickle.load(open('model.pkl', 'rb'))
 cv = pickle.load(open('cv-transform.pkl','rb'))
 
-@app.route("/")
+@app.route('/')
 def home():
-    return render_template("home.html")
+	return render_template('home.html')
 
 
-@app.route("/predict", methods = ["POST"])
+@app.route('/predict',methods=['POST'])
 def predict():
-    if request.method == "POST":
-        message = request.form['message']
+    if request.method == 'POST':
+    	message = request.form['message']
     	data = [message]
     	vect = cv.transform(data).toarray()
-    	my_prediction = classifier.predict(vect)   
-        return render_template('prediction.html',prediction=my_prediction)
+    	my_prediction = classifier.predict(vect)
+    	return render_template('prediction.html', prediction=my_prediction)
 
-if __name__ == "__main__":
+if __name__ == '__main__':
     app.run(debug=True)
