f1a24d3034ec2cb3ea1dfeb18056d6453b8488ff
shrutibalan4591 <77207245+shrutibalan4591@users.noreply.github.com>
2021-06-08 12:33:15 +0530

    Update app.py

--- app.py
+++ app.py
@@ -19,15 +19,14 @@ def home():
     return render_template("home.html")
 
 
-@app.route("/predict", methods = ["GET", "POST"])
+@app.route("/predict", methods = ["POST"])
 def predict():
     if request.method == "POST":
-      message = request.form['message']
+        message = request.form['message']
     	data = [message]
     	vect = cv.transform(data).toarray()
     	my_prediction = classifier.predict(vect)   
-
-      return render_template('prediction.html',prediction_text=my_prediction)
+        return render_template('prediction.html',prediction=my_prediction)
 
 if __name__ == "__main__":
     app.run(debug=True)
