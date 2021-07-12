f92e73f0708b946f3164d2b0d08d3cb067282b62
shrutibalan4591 <77207245+shrutibalan4591@users.noreply.github.com>
2021-07-13 00:15:41 +0530

    Update app.py

--- app.py
+++ app.py
@@ -16,7 +16,7 @@ def predict():
     	message = request.form['message']
     	data = [message]
     	vect = cv.transform(data).toarray()
-    	my_prediction = classifier.predict(vect)
+    	my_prediction = model.predict(vect)
     	return render_template('prediction.html', prediction=my_prediction)
 
 if __name__ == '__main__':
